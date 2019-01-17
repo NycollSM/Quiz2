15dcd82b91b0ad11477e44e30d40ae93c411b34f
NycollSM <nycollsotomorales21@gmail.com>
2019-01-17 16:50:31 -0600

    arrows done

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const left = document.getElementById('arrow-left');
+const right = document.getElementById('arrow-right');
+
+function test() {
+  alert('b wede00');
+}
+left.addEventListener('click', test);
