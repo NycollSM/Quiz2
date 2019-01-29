24dea32c5c641cf7c9e314f9030149bfc50a3b6d
NycollSM <nycollsotomorales21@gmail.com>
2019-01-28 18:57:05 -0600

    agregando elementos al dom

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
-const left = document.getElementById('arrow-left');
-const right = document.getElementById('arrow-right');
+const left = $('#left');
+const right = $('#right');
+const eventS = $('.event');
+const data = null;
+$.ajax({
+    url: '../json/event-api.json',
+    type: 'GET',
+    dataType: 'json',
+    success: function data (response){
+            //console.log(response.events[0].id);
+            for (const i of response.events){
+                const liItem = $('<li><li>');
+                // date
+                const title = $('<h2>').text(i.hardcoded);
+                // subtitle
+                const subtitles = $('<p>').addClass('subtitles').text(i.name.text);
+                // location
+                const locationTxt = $('<p>').addClass('locations').text(i.venue.name);
+                const btn = $('<button>').addClass('btn').attr('src','_blanck',i.url).text('Get Event Details');
+                // adding elements at dom
+                eventS.append(liItem); 
+                liItem.append(title);
+                liItem.append(subtitles);
+                liItem.append(locationTxt);
+                liItem.append(btn);
+            }
+        }
+        
+    });
+$(document).ready('load', data);
 
-function test() {
-  alert('b wede00');
-}
-left.addEventListener('click', test);
+
+
