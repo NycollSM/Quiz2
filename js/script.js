8eba3e5138906d8fa0cbf8357a7c3d1cd37583cb
NycollSM <nycollsotomorales21@gmail.com>
2019-04-02 16:49:18 -0600

    implementing BEM

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
 const left = $('#left');
 const right = $('#right');
-const eventS = $('.event');
+const events = $('.event__info');
 const data = null;
 $.ajax({
     url: '../json/event-api.json',
     type: 'GET',
     dataType: 'json',
     success: function data (response){
-            //console.log(response.events[0].id);
-            for (const i of response.events){
-                const liItem = $('<li><li>');
-                // date
-                const title = $('<h2>').text(i.hardcoded);
-                // subtitle
-                const subtitles = $('<p>').addClass('subtitles').text(i.name.text);
-                // location
-                const locationTxt = $('<p>').addClass('locations').text(i.venue.name);
-                const btn = $('<button>').addClass('btn').attr('src','_blanck',i.url).text('Get Event Details');
-                // adding elements at dom
-                eventS.append(liItem); 
-                liItem.append(title);
-                liItem.append(subtitles);
-                liItem.append(locationTxt);
-                liItem.append(btn);
-            }
+        for (const i of response.events){
+            console.log(i);
+            const liItem = $('<li><li>');
+            // date
+            const title = $('<h2>');
+            title.text(i.hardcoded);
+            // subtitle
+            const subtitles = $('<p>').addClass('event__subtitle');
+            subtitles.text(i.name.text);
+            // location
+            const locationTxt = $('<p>').addClass('event__location');
+            locationTxt.text(i.venue.name);
+            const btn = $('<button>').addClass('event__button').attr('src','_blanck',i.url)
+            btn.text('Get Event Details');
+            // adding elements at dom
+            events.append(liItem); 
+            liItem.append(title);
+            liItem.append(subtitles);
+            liItem.append(locationTxt);
+            liItem.append(btn);
+        }
         }
         
     });
